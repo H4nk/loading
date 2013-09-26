fc7de722e4bac83ba5e408fbfb05a1e2287fae50
Harry Setya Hadi <xmoensen@gmail.com>
2013-09-26 08:30:46 +0700

    Create loading.php

---
+++ loading.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200' rel='stylesheet' type='text/css'>
+	<style>
+		*{
+			padding: 0;margin: 0;
+		}
+		a{
+			text-decoration: none;
+			color: #333;
+		}
+		body{
+			background:#ddd url('http://subtlepatterns.com/patterns/textured_paper.png');
+			font-family: 'Titillium Web', sans-serif;
+		}
+		iframe{
+			margin: 20px 0;
+		}
+		h1{
+			text-align: center;
+		}
+		h2{
+			margin: 15px 0;
+			line-height: 1em;
+			font-size: 17px;
+		}
+		p{
+			margin:10px 0;
+		}
+		#loading {
+			position: fixed;
+			left: 0px;
+			top: 0px;
+			width: 100%;
+			height: 100%;
+			z-index: 9999;
+			background: url(http://1.bp.blogspot.com/-WzSnoUguCBc/UTftCm0wM7I/AAAAAAAACD4/1ebnSoA5LV4/s1600/loading51.gif) 50% 50% no-repeat #fff;
+		}
+		.container{
+			width: 730px;
+			margin: 0 auto;
+			padding: 20px;
+			background: #fff;
+		}
+			header{
+ 
+			}
+			.content img{
+				width: 750px;
+				height: 550px;
+			}
+	</style>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>
+	<script type="text/javascript">
+		$(window).load(function() { $("#loading").fadeOut("slow"); })
+	</script>
+ 
+</head>
+<body>
+ 
+	<div id="loading"></div>
+ 
+	<div class="container">
+			<section>
+				<div class="content">
+					<img src="http://3.bp.blogspot.com/-RTmA0Rton9M/Ty-pMEkXdSI/AAAAAAAAA9E/Zuoifd24NlI/s640/Keindahan+pariwisata+kota+padang+versi+h4nk_1.jpg" alt="">
+					<img src="http://4.bp.blogspot.com/-LVz-sGgKHcY/Ty-pN1E3f1I/AAAAAAAAA9M/YSeoqvq6O9g/s640/Keindahan+pariwisata+kota+padang+versi+h4nk_2.jpg" alt="">
+					<img src="http://4.bp.blogspot.com/-5BBi_ppr2Ac/Ty-pO8WKoVI/AAAAAAAAA9U/zAo6BueI7do/s640/Keindahan+pariwisata+kota+padang+versi+h4nk_3.jpg" alt="">
+					<img src="http://3.bp.blogspot.com/-pldHj_ggFnU/Ty-pef6eHtI/AAAAAAAAA9c/f7BxFUn4I3M/s640/pantai+air+manis.JPG" alt="">
+					<img src="http://2.bp.blogspot.com/-mkUc5K1GZJk/Ty-rIRnYzjI/AAAAAAAAA9k/7-ECiTiWZh4/s640/Pantai_padang_1.jpg" alt="">
+					<img src="http://1.bp.blogspot.com/-Tt-jekDsE2w/Ty-rJIbAdRI/AAAAAAAAA9s/_KdU9put3xA/s640/Pantai_padang_2.jpg" alt="">
+					<img src="http://4.bp.blogspot.com/--ITod1yd0kc/Ty-rKJofEoI/AAAAAAAAA9w/rN7W7ALl5Ek/s640/Pantai_padang_3.JPG" alt="">
+					<img src="http://3.bp.blogspot.com/-VHu6QXMh3JA/Ty-rMOuGD1I/AAAAAAAAA98/9Me1Hj8FHAc/s640/Pantai_padang_4.jpg" alt="">
+					<img src="http://1.bp.blogspot.com/-IsDFHs0g3Go/Ty-tIL8WD_I/AAAAAAAAA-M/PoDyd0A34cw/s640/Museum+Adityawarman_1.JPG" alt="">
+					<img src="http://4.bp.blogspot.com/-SB5k3sBFA_U/Ty-tLxkpo8I/AAAAAAAAA-c/UFelVb21Xew/s640/Museum+Adityawarman_3.jpg" alt="">
+					<img src="http://1.bp.blogspot.com/--GYx1bZmWMA/Ty-v6tjD0MI/AAAAAAAAA_c/ANaWOslgPOA/s640/Jembatan+Siti+Nurbaya_3.jpg" alt="">
+					<img src="http://4.bp.blogspot.com/-JbnOEj_Rxn0/Ty-xc3EyxrI/AAAAAAAABAE/sTJDBF-5lXQ/s640/Pantai+pasir+Jambak_2.jpg" alt="">
+					<img src="http://4.bp.blogspot.com/--hxaEYblmmI/Ty-xgWVEhEI/AAAAAAAABAU/1QvjB4-0fxc/s640/Pantai+pasir+Jambak_4.JPG" alt="">
+					<img src="http://2.bp.blogspot.com/-Ghbj8jHTT8A/Ty-xiB8biUI/AAAAAAAABAk/lKwLN2jIgYQ/s640/Pantai+pasir+Jambak_6.jpg" alt="">
+					<img src="http://3.bp.blogspot.com/-22oOy3K_OU8/Ty-zbHkhZ_I/AAAAAAAABBI/PZSZk91tfaU/s640/Pantai+Nirwana_1.JPG" alt="">
+					<img src="http://3.bp.blogspot.com/--JJ1Fwc6qpQ/Ty-zctcaPuI/AAAAAAAABBQ/e5Au1J1xloI/s640/Pantai+Nirwana_2.jpg" alt="">
+					<img src="http://1.bp.blogspot.com/-l0AvF1m_f18/Ty-zfLUxwMI/AAAAAAAABBo/GPouSFd8dKU/s640/Pantai+Nirwana_5.jpg" alt="">
+					<img src="http://3.bp.blogspot.com/-8eAjxIL32Ro/Ty-0_OlfYlI/AAAAAAAABB4/olQcDkJeXsg/s640/Pantai+Caroline_2.jpg" alt="">
+ 
+				</div> 
+		</section> 
+ 
+	</div> 
+ 
+</body>
+</html>
